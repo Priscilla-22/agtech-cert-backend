08776b3074e7cd3f150649cc44c19e600723fd3d
wakahia <priscilla.w.dev@gmail.com>
2025-09-16 14:51:16 +0300

    feat:farm management

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const { testConnection } = require('./config/database');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3002;
 
 // Middleware
 app.use(helmet());
